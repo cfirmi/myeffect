a2306ea834ed5376602ce78368f7f6f2016f1380
Christian Firmi <cpalvetzian@gmail.com>
2019-06-09 23:24:09 -0400

    commit from macbook

---
+++ myeffect-app/frontend/components/buttons/objective/joinbutton.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+
+const joinbutton = () => (
+  <div>
+    Join 
+    {/* HI */}
+    hello
+  </div>
+)
+
+export default joinbutton
